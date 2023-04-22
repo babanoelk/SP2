4b6fcf2e1cd9721e9aca853869a59ebc663af103
babanoelk <124783309+babanoelk@users.noreply.github.com>
2023-04-22 22:09:00 +0200

    Rettelser

--- SP2/src/carProgram/Main.java
+++ SP2/src/carProgram/Main.java
@@ -4,10 +4,10 @@ public class Main {
     public static void main(String[] args) {
 
 
-        DieselCar Touran = new DieselCar("AJ 22 394", "Volkswagen", "Touran", 5, 6, "Diesel", false);
-        GasolinCar SyvSerie = new GasolinCar("KJ 01 002", "BMW", "7-series", 5, 5, "Benzin");
-        ElectricCar Tesla = new ElectricCar("NX 23 392", "Tesla", "Serie X", 5, 103000, 350);
-        GasolinCar Ferrari488 = new GasolinCar("IS 92 102", "Ferrari","488",3,2,"Benzin");
+        Car Touran = new DieselCar("AJ 22 394", "Volkswagen", "Touran", 5, 6, "Diesel", false);
+        Car SyvSerie = new GasolinCar("KJ 01 002", "BMW", "7-series", 5, 5, "Benzin");
+        Car Tesla = new ElectricCar("NX 23 392", "Tesla", "Serie X", 5, 103000, 350);
+        Car Ferrari488 = new GasolinCar("IS 92 102", "Ferrari","488",3,2,"Benzin");
 
         FleetOfCars carList = new FleetOfCars();
 
